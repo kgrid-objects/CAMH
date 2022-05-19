36e831a00a9b8c6fa8c3cbdaee8a4966da864894
Shreya Kapoor <89872698+Shreyakapoor26@users.noreply.github.com>
2022-05-19 13:12:00 -0400

    latest

--- collection/99999-CAMH1-v1.0/Index.js
+++ collection/99999-CAMH1-v1.0/Index.js
@@ -3,21 +3,23 @@
  * FILENAME : index.js 
  * AUTHOR   : Dr. Allen Flynn and Shreya Kapoor, April, 2022
  * 
- * Summary  : Program for CAMH hospital to take questionnaire input and do some
- * analysis and return result.
+ * Summary  : Paload file for CAMH hospital to take questionnaire input and do some
+ * analysis and return interpretation and recommendations.
  * 
- * Content  :
-    * PHQ9_Questionnaire
-    * Input Object(i)
-    * Main Function call : process_request(i(takes in input request)){}
-    * Functions -->
-        1)PHQ9_Score_From_Array(Input: ){Response:}
-        2)PHQ9_Score_Advice(Input: takes pt. current PHQ Score) {Response: see if the number is between 0-27; if valid PHQ9 score, provides interpretation and recommendations}
-        3)Attach_PHQ9 (Input: Request for questionnaire(yes/no)){Response: if yes-send the questionaire; if no-do nothing}
  * =================================================================================+ */
 
+const EXIT_SUCCESS = 0;
+const EXIT_FAILURE = 1;
+        
+const MIN_PHQ9_SCORE = 0;
+const MAX_PHQ9_SCORE = 27;
+        
+const MIN_ANSWER_ARRAY_LEN = 0;
+const MAX_ANSWER_ARRAY_LEN = 9;
+
+//------------------------------------------------------------------//
 // PHQ9_Questionnaire
-const PHQ9_Questionnaire = {
+const PHQ9_Content = {
     initialPrompt: "Over the last 2 weeks, how often have you been bothered by any of the following problems?",
     questionOne: "Little interest or pleasure in doing things: ",
     questionTwo: "Feeling down, depressed, or hopeless: ",
@@ -29,63 +31,103 @@ const PHQ9_Questionnaire = {
     questionEight: "Moving or speaking so slowly that other people could have noticed? Or the opposite — being so fidgety or restless that you have been moving around a lot more than usual: ",
     questionNine: "Thoughts that you would be better off dead or of hurting yourself in some way: ",
 }
-
-//-----------------------------------------------------------//
-//Main Function call
-function process_request(i) {
-    var o = {};
-    var Score = PHQ9_Score_From_Array(i.Answers_Array);
-    if (Score.PHQ9_total_from_array < i.Total_PHQ9_Score) {
-        Score.PHQ9_total_from_array = i.Total_PHQ9_Score;
+        
+//------------------------------------------------------------------//
+//Sanitize request functions
+//Sanitize Total PHQ9 Score input function
+function sanitize_input_Total_PHQ9_Score(raw_request) {
+        
+    // Confirming the datatype of request fields are correct.
+    if (typeof raw_request.Total_PHQ9_Score != "number" ||
+        typeof raw_request == null) {
+                
+        return {error_code: EXIT_FAILURE, error_msg: "Wrong datatype provided! Please check again"};
     }
-    o.Score = Score;
-    o.Interpretation_And_Recommendations = PHQ9_Score_Advice(Score.PHQ9_total_from_array);
-    o.PHQ9_Questionnaire = Attach_PHQ9(i.Send_Questionnaire);
-    return o
+        
+    // Confirming the bounds of total output.Total_PHQ9_Score input
+    if (raw_request.Total_PHQ9_Score < MIN_PHQ9_SCORE ||
+        raw_request.Total_PHQ9_Score > MAX_PHQ9_SCORE) {
+                
+        return {error_code: EXIT_FAILURE, error_msg: "PHQ9 total output.Total_PHQ9_Score not in accepted range! Please check again"};
+        }
+        
+    // If input is correct, return EXIT_SUCCESS
+        return {error_code: EXIT_SUCCESS, error_msg: "No error"};
 }
 
-//console.log(process_request(i));
-//module.exports = { process_request };
+//Sanitize array input function
+function sanitize_Answers_Array(raw_request) {
+        
+    // Confirming the datatype of request fields are correct.
+    if (typeof raw_request.Answers_Array != "object" ||
+        typeof raw_request == null) {
+                
+        return {error_code: EXIT_FAILURE, error_msg: "Wrong datatype provided! Please check again"};
+    }
+        
+    // Confirming the answer array length is within bounds
+    if (raw_request.Answers_Array.length < MAX_ANSWER_ARRAY_LEN ||
+        raw_request.Answers_Array.length > MAX_ANSWER_ARRAY_LEN) {
+            
+        return {error_code: EXIT_FAILURE, error_msg: "Answer array length not in accepted range! Please check again"};
+    }
+        
+    // Confirming if the values in answer array are in range 0-3
+    for (let ques_no = 0; ques_no < raw_request.Answers_Array.length; ques_no++) {
+        if(raw_request.Answers_Array[ques_no] < 0 || raw_request.Answers_Array[ques_no] > 3) {
+        
+        return {error_code: EXIT_FAILURE, error_msg: "Answer to question " + (ques_no + 1) + " not in accepted range! Please check again"};
+        }
+    }
+            
+    // If input is correct, return EXIT_SUCCESS
+        return {error_code: EXIT_SUCCESS, error_msg: "No error"};
+    }
 
-//-----------------------------------------------------------//
-//Functions 1
-//Testing the value for the request for the questionnaire
+//Sanitize send questionaire request input function
+function sanitize_Send_Questionnare(raw_request) {
+        
+    // Confirming the datatype of request fields are correct.
+    if (typeof raw_request.Send_Questionnare != "string" ||
+        typeof raw_request.Send_Questionnare == "" ||
+        typeof raw_request == null) {
+                
+        return {error_code: EXIT_FAILURE, error_msg: "Wrong datatype provided! Please check again"};
+        }
+        
+    // Confirming the values Send_Questionnare input to be yes/no
+    else if (raw_request.Send_Questionnare.toUpperCase() != "YES" &&
+        raw_request.Send_Questionnare.toUpperCase() != "NO") {
+                
+        return {error_code: EXIT_FAILURE, error_msg: "Send_Questionnare value should be yes or no. Please check again"};
+        }
+            
+    // If input is correct, return EXIT_SUCCESS
+        return {error_code: EXIT_SUCCESS, error_msg: "No error"};
+    }
+        
+//------------------------------------------------------------------//
+//Function testing the value for the request for the questionnaire
 function Attach_PHQ9(Send_Questionnaire) {
     var PHQ9_Questionnaire = {};
     if (String(Send_Questionnaire).toUpperCase() == "YES") {
-        PHQ9_Questionnaire = PHQ9content;
+        PHQ9_Questionnaire = PHQ9_Content;
     }
     else if (String(Send_Questionnaire).toUpperCase() == "NO") {
         PHQ9_Questionnaire = "Not requested";
     }
-    else if (Send_Questionnaire == "") {
-        PHQ9_Questionnaire = "Not requested";
-    }
     else {
-        PHQ9_Questionnaire = "Request not understood";
+    console.log(Send_Questionnaire)
+        PHQ9_Questionnaire = "Invalid request value"
     }
     return PHQ9_Questionnaire
-}
-
-//Functions 2
-//testing the validity of PHQ9 score and providing interpretations and recommendations
+}       
+        
+//Function testing the validity of PHQ9 score and providing interpretations and recommendations
 //Interprertation url: https://www.hiv.uw.edu/page/mental-health-screening/phq-9
-
 function PHQ9_Score_Advice(score) {
     var Interpretation_And_Recommendations = {};
-    if (typeof score === "string") {
-        Interpretation_And_Recommendations = {
-            Depression_Severity: "Undetermined due to string input",
-            Proposed_Treatment_Actions: "Undetermined due to string input"
-        }
-    }
-    else if (score === null) {
-        Interpretation_And_Recommendations = {
-            Depression_Severity: "Undetermined",
-            Proposed_Treatment_Actions: "Undetermined"
-        }
-    }
-    else if (score >= 0 && score <= 4) {
+    if (score >= 0 && score <= 4) {
         Interpretation_And_Recommendations = {
             Depression_Severity: "None_minimal",
             Proposed_Treatment_Actions: "None"
@@ -115,17 +157,11 @@ function PHQ9_Score_Advice(score) {
             Proposed_Treatment_Actions: "Immediate initiation of pharmacotherapy and, if severe impairment or poor response to therapy, expedited referral to a mental health specialist for psychotherapy and/or collaborative management"
         }
     }
-    else {
-        Interpretation_And_Recommendations = {
-            Depression_Severity: "Undetermined due to number out of range",
-            Proposed_Treatment_Actions: "Undetermined due to number out of range"
-        }
-    }
     return Interpretation_And_Recommendations
-}
-
-//Functions 3
-//testing the array total and calculating the sum
+}              
+        
+        
+//Function testing the array total and calculating the sum
 function sumarray(array) {
     var sum = 0;
     for (let i = 0; i < array.length; i++) {
@@ -143,19 +179,7 @@ function onlyNumbers(array) {
 function PHQ9_Score_From_Array(Answers) {
     var Score = {};
     var count_input_in_array = Answers.length;
-    if (count_input_in_array < 9) {
-        Score = {
-            Array_Length: "PHQ9 question input has less than 9 answers",
-            PHQ9_total_from_array: "Could not be determined"
-        }
-    }
-    else if (count_input_in_array > 9) {
-        Score = {
-            Array_Length: "PHQ9 question input has more than 9 answers",
-            PHQ9_total_from_array: "Could not be determined"
-        }
-    }
-    else if (count_input_in_array == 9 && onlyNumbers(Answers)) {
+    if (count_input_in_array == 9 && onlyNumbers(Answers)) {
         Score = {
             Array_Length: 9,
             PHQ9_total_from_array: sumarray(Answers)
@@ -168,5 +192,29 @@ function PHQ9_Score_From_Array(Answers) {
         }
     }
     return Score
+}      
+
+
+//-----------------------------------------------------------//
+//Main Function call
+function process_request(request) {
+    var main_output = {};
+    var Score = PHQ9_Score_From_Array(request.Answers_Array);
+    var Sanitized_Total_PHQ9_Score = sanitize_input_Total_PHQ9_Score(request);
+    var Sanitized_Answers_Array = sanitize_Answers_Array(request);
+    var Sanitized_Send_Questionnare = sanitize_Send_Questionnare(request);
+
+    if (Sanitized_Total_PHQ9_Score.error_code == EXIT_FAILURE) return  Sanitized_Total_PHQ9_Score;
+    else if (Sanitized_Answers_Array.error_code == EXIT_FAILURE) return Sanitized_Answers_Array;
+    else if (Sanitized_Send_Questionnare.error_code == EXIT_FAILURE) return Sanitized_Send_Questionnare;
+    else if (Score.PHQ9_total_from_array < request.Total_PHQ9_Score) {
+        Score.PHQ9_total_from_array = request.Total_PHQ9_Score;
+    }
+    main_output.Score = Score;
+    main_output.Interpretation_And_Recommendations = PHQ9_Score_Advice(Score.PHQ9_total_from_array);
+    main_output.PHQ9_Questionnaire = Attach_PHQ9(request.Send_Questionnare);
+    return main_output
 }
 
+module.exports = { process_request };       
+        
--- collection/99999-CAMH1-v1.0/test.js
+++ collection/99999-CAMH1-v1.0/test.js
@@ -3,81 +3,100 @@
 //import * as process from 'F1_PHQ_questionnaire.js';
 var api = require('./index');
 
+const EXIT_SUCCESS = 0;
+const EXIT_FAILURE = 1;
+
+function error_handling(response) {
+    
+    if (response.error_code == EXIT_FAILURE) {
+        console.log("Test failed");
+        console.log(response.error_msg);
+    } else {
+        console.log("Expected output recieved");
+        console.log(response);
+    }
+}
+
 /* --------------------------------------------------------------------------------------------------------------- */
 
 // TEST 0 - Sending a correct request
-var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 3], Send_Questionnare: "Yes"};
+console.log("\nTEST 0 - Sending a correct request");
+var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 3], Send_Questionnare: "YES"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 /* -------------------------- */
 
 // TEST 1 - Testing for wrong datatype -->
 // TEST 1.1 Total_PHQ9_Score field is wrongly sent as string
+console.log("\nTEST 1.1 Total_PHQ9_Score field is wrongly sent as string");
 var request = {Total_PHQ9_Score: "27", Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 3], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 // TEST 1.2 Answers_Array field is wrongly sent as string
+console.log("\nTEST 1.2 Answers_Array field is wrongly sent as string");
 var request = {Total_PHQ9_Score: 27, Answers_Array: "[1, 0, 2, 3, 2, 1, 1, 2, 3]", Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 // TEST 1.3 Send_Questionnaire field is wrongly sent as number
+console.log("\nTEST 1.3 Send_Questionnaire field is wrongly sent as number");
 var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 3], Send_Questionnare: 2};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 /* -------------------------- */
 
 // TEST 2 - Testing for total PHQ9 score bounds
 // TEST 2.1 - If Total score is less than 0
+console.log("\nTEST 2.1 - If Total score is less than 0");
 var request = {Total_PHQ9_Score: -5, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 3], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
-console.log("TEST 2.2 - If Total score is greater than 27")
+// TEST 2.2 - If Total score is greater than 27"
+console.log("\nTEST 2.2 - If Total score is greater than 27");
 var request = {Total_PHQ9_Score: 30, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 3], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-if (response.Score.PHQ9_total_from_array > 27) {
-    console.log("Passed")
-}
-else {
-    console.log("Failed: Expecting 27. Got: ", response.Score.PHQ9_total_from_array);
-    console.log(response);
-}
-
+error_handling(response);
 
 /* -------------------------- */
 
 // TEST 3 - Testing for array length bounds
 // TEST 3.1 - If array length is less than 0
+console.log("\nTEST 3.1 - If array length is less than 0");
 var request = {Total_PHQ9_Score: 27, Answers_Array: [], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 // TEST 3.2 - If array length is greater than 9
+console.log("\nTEST 3.2 - If array length is greater than 9");
 var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 2, 2, 3, 2, 1, 1, 2, 3, 1], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 /* -------------------------- */
 
 // TEST 4 - testing for values in answer array are in range 0-3
 // TEST 4.1 - If value(s) is/are less than 0
+console.log("\nTEST 2.2 - If Total score is greater than 27");
 var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 0, -2, 3, 2, 1, 1, 2, 2], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 // TEST 4.2 - If value(s) is/are greater than 9
+console.log("\nTEST 2.2 - If Total score is greater than 27");
 var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 10], Send_Questionnare: "Yes"};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
 
 /* -------------------------- */
 
 // TEST 5 - testing for value(s) in send questionnaire is/are yes or no
 // TEST 5.1 - If value(s) is/are other than yes/no
+console.log("\nTEST 5.1 - If value(s) is/are other than yes/no");
 var request = {Total_PHQ9_Score: 27, Answers_Array: [1, 0, 2, 3, 2, 1, 1, 2, 2], Send_Questionnaire: ""};
 var response = api.process_request(request);
-console.log(response);
+error_handling(response);
+
