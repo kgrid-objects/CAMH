f20091023a08263c043c632dac0822466c64b490
ajflynn <ajflynn@umich.edu>
2023-06-24 13:58:36 -0400

    Shexvalidation added
    shexvalidation

---
+++ 624ShExValidation-main/script_gen_script/valman.sh
@@ -0,0 +1,42 @@
+# 5/18/2023 (Updated)
+# validates using Shex.js parser
+
+# First location of the shex.js parser 
+SHEXJS=~/A123/git/research/FHIRCat/shex.js
+VALIDATE=$SHEXJS/packages/shex-cli/bin
+
+set -x
+export PATH=$VALIDATE:$PATH
+
+# One way to invoke validate with YAML FILE
+# YAML=./FunctionPatterns/FunctionPatterns.yaml
+YAML=../fhir_rdf_validation/Account.yaml
+
+# validate --help
+
+# validate --verbose --human --yaml-manifest $YAML
+
+# Another way of invoking validate with SHEX and TTL
+SHEX=../fhir_rdf_validation/ShExSchemas/R5Plus/Account.shex
+#SHEX=../fhir_rdf_validation/ShExSchemas/R5PlusWithConstraints/Account.shex
+TTL=../fhir_rdf_validation/FHIR_RDF_Examples/R5/account-example.ttl
+
+# validate shex with a ttl
+# validate --human -x $SHEX -d $TTL -m '{FOCUS a fhir:Account}@<Account>'
+TTL=../fhir_rdf_validation/FHIR_RDF_Examples/R5/account-example-with-guarantor.ttl
+# validate --human -x $SHEX -d $TTL -m '{FOCUS a fhir:Account}@<Account>'
+
+# validate for a select query map
+# validate -x $SHEX -d $TTL -m '<inst_exists>@<FunctionPatternsShape>,<inst_noExists>@!<FunctionPatternsShape>'
+
+# run it as a server
+# validate -x ../fhir_rdf_validation/ShExSchemas/R5Plus/Account.shex --diagnose  --serve http://localhost/validate || echo fail
+validate --human -x $SHEX --diagnose --serve http://localhost:8086/validate --skipCycleCheck
+
+# example to check if ShEx schema has any error
+# for example if they have duplicate shapes
+# validate -x packages/shex-cli/test/Imports/TrompPersonMissingRep/Issue.shex --diagnose --dry-run || echo fail
+
+# validate -x ../fhir_rdf_validation/ShExSchemas/R5Plus/Account.shex --dry-run --diagnose  || echo fail
+
+set +x
